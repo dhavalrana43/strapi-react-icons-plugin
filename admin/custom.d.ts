4166e072c96030bee37b8b4f8c167e826722cde4
Dhaval Rana <drana@plusonex.com>
2025-01-31 16:39:56 +0530

    all error resolved

--- admin/custom.d.ts
+++ admin/custom.d.ts
@@ -2,3 +2,6 @@ declare module '@strapi/design-system/*';
 declare module '@strapi/design-system';
 declare module '@strapi/icons';
 declare module '@strapi/icons/*';
+declare module '@strapi/icons' {
+    export * from '@strapi/icons/dist/index';
+  }
